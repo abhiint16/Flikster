94f60793b1aacdca8e9efdf849c391211df7128f
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-04 12:21:01 +0530

    initializing views

--- app/src/main/java/com/flikster/FeedFragment.java
+++ app/src/main/java/com/flikster/FeedFragment.java
@@ -3,6 +3,9 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,10 @@ import android.view.ViewGroup;
 
 public class FeedFragment extends Fragment {
     View view;
+    RecyclerView feed_mainRecyclerView;
+    RecyclerView.LayoutManager feedLayoutManager;
+    FeedRecyclerAdapter feedAdapter;
+    FragmentManager fragmentManager;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,8 +30,14 @@ public class FeedFragment extends Fragment {
     }
 
     private void initializeRest() {
+        feedLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        feed_mainRecyclerView.setLayoutManager(feedLayoutManager);
+        feedAdapter = new FeedRecyclerAdapter(getActivity(),fragmentManager);
+        feed_mainRecyclerView.setAdapter(feedAdapter);
     }
 
     private void initializeViews() {
+        feed_mainRecyclerView=(RecyclerView)view.findViewById(R.id.recyclerview);
+        fragmentManager=getActivity().getSupportFragmentManager();
     }
 }
---
+++ app/src/main/java/com/flikster/FeedRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.flikster;
+
+/**
+ * Created by abhishek on 04-10-2017.
+ */
+
+public class FeedRecyclerAdapter {
+}
