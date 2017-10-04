60865e7de58a92812ac8babab2566d8b806d9d9d
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-04 11:59:59 +0530

    initializing views and setting clikc listener

--- app/src/main/java/com/flikster/HomeActivity.java
+++ app/src/main/java/com/flikster/HomeActivity.java
@@ -3,17 +3,35 @@ package com.flikster;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class HomeActivity extends AppCompatActivity implements  FragmentChangeInterface{
+public class HomeActivity extends AppCompatActivity implements  FragmentChangeInterface, View.OnClickListener {
 
+    LinearLayout feed,rating,plus,fashion,store;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initializeViews();
+        initializeRest();
+    }
+
+    private void initializeRest() {
+        feed.setOnClickListener(this);
+    }
+
+    private void initializeViews() {
+        feed=(LinearLayout)findViewById(R.id.feed_button);
     }
 
     @Override
     public void beginTransaction1(Fragment fragment) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
         
     }
 }
