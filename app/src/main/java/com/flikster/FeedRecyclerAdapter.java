998dff4310d639c7b9c9ff0f23095c7b6de74ae0
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-04 12:23:57 +0530

    initializing FeedRecyclerAdapter

--- app/src/main/java/com/flikster/FeedRecyclerAdapter.java
+++ app/src/main/java/com/flikster/FeedRecyclerAdapter.java
@@ -1,8 +1,42 @@
 package com.flikster;
 
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by abhishek on 04-10-2017.
  */
 
-public class FeedRecyclerAdapter {
+public class FeedRecyclerAdapter extends RecyclerView.Adapter<FeedRecyclerAdapter.ViewHolder>{
+    
+    Context context;
+    FragmentManager fragmentManager;
+    public FeedRecyclerAdapter(Context context, FragmentManager fragmentManager) {
+        this.context=context;
+        this.fragmentManager=fragmentManager;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
