b9ae4d546c257bd23f806d354d0f85cb91881473
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-04 11:51:12 +0530

    implementing interface in the HoomeActivity

--- app/src/main/java/com/flikster/HomeActivity.java
+++ app/src/main/java/com/flikster/HomeActivity.java
@@ -1,13 +1,19 @@
 package com.flikster;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements  FragmentChangeInterface{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void beginTransaction1(Fragment fragment) {
+        
     }
 }
