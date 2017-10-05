3b622b853cdd6724543ce70e790e3846d417ba41
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-05 16:05:04 +0530

    initiazling and setting adapter for recyclerview

--- app/src/main/java/com/flikster/GallaryCardClick.java
+++ app/src/main/java/com/flikster/GallaryCardClick.java
@@ -3,6 +3,8 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +15,26 @@ import android.view.ViewGroup;
 
 public class GallaryCardClick extends Fragment {
     View view;
+    RecyclerView gallary_recyclerview;
+    RecyclerView.LayoutManager gallaryLayoutManager;
+    GallaryCardClickAdapter gallaryCardClickAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.card_gallary_item_onclick,container,false);
+        initializeViews();
+        initializeRest();
         return view;
+    }
+
+    private void initializeRest() {
+        gallaryLayoutManager=new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        gallary_recyclerview.setLayoutManager(gallaryLayoutManager);
+        gallaryCardClickAdapter=new GallaryCardClickAdapter(getActivity());
+        gallary_recyclerview.setAdapter(gallaryCardClickAdapter);
+    }
+
+    private void initializeViews() {
+        gallary_recyclerview=(RecyclerView)view.findViewById(R.id.gallary_recyclerview);
     }
 }
