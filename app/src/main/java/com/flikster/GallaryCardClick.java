fa3d0d2eb600ac6936e965a57a153cd6e69e8674
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-05 15:29:27 +0530

    adding GallaryCardClick fragment

---
+++ app/src/main/java/com/flikster/GallaryCardClick.java
@@ -0,0 +1,22 @@
+package com.flikster;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abhishek on 05-10-2017.
+ */
+
+public class GallaryCardClick extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.card_gallary_item_onclick,container,false);
+        return view;
+    }
+}
