95b968005825a55b7c843c18742b19c36c12ff0f
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-05 15:43:35 +0530

    adding GlobalData class that contains data

---
+++ app/src/main/java/com/flikster/GlobalData.java
@@ -0,0 +1,26 @@
+package com.flikster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by abhishek on 05-10-2017.
+ */
+
+public class GlobalData {
+
+    List<Integer> type=new ArrayList<>();
+    List<String> imag=new ArrayList<>();
+
+    public GlobalData() {
+        type.add(4);
+        type.add(3);type.add(4);type.add(2);type.add(4);type.add(1);type.add(2);type.add(2);type.add(1);type.add(3);type.add(4);
+        type.add(3);type.add(4);type.add(7);type.add(4);type.add(9);type.add(3);type.add(4);type.add(1);type.add(2);type.add(4);
+        type.add(4);type.add(8);type.add(5);
+        imag.add("http://img.youtube.com/vi/MeH346YHUIE/0.jpg");imag.add("http://img.youtube.com/vi/CUYcVfVt88I/0.jpg");
+        imag.add("http://img.youtube.com/vi/IkIqgTt8Xsk/0.jpg");
+        imag.add("http://img.youtube.com/vi/nwJ0tL8Fi-E/0.jpg");imag.add("http://img.youtube.com/vi/lhwfWm-m7tw/0.jpg");
+        imag.add("http://img.youtube.com/vi/-0XiiT5dR_Q/0.jpg");imag.add("http://img.youtube.com/vi/FXAngnH3s0c/0.jpg");
+        imag.add("http://img.youtube.com/vi/rJwPCq61qfo/0.jpg");imag.add("http://img.youtube.com/vi/9gwNKVnmVIg/0.jpg");
+    }
+}
