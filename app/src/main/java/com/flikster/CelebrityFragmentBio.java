7d62337df5d65ea0a19b6b7126303372921e0027
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-13 12:34:50 +0530

    adding celebrity_bio working

--- app/src/main/java/com/flikster/CelebrityFragmentBio.java
+++ app/src/main/java/com/flikster/CelebrityFragmentBio.java
@@ -3,6 +3,9 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +16,27 @@ import android.view.ViewGroup;
 
 public class CelebrityFragmentBio extends Fragment{
     View view;
+    RecyclerView celebrityFragmentBioRecycler;
+    RecyclerView.LayoutManager celebrityFragmentBioLayoutManager;
+    CelebrityBioAdapter celebrityBioAdapter;
+    FragmentManager fragmentManager;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_celebrity_bio,container,false);
+        initializeViews();
+        initializeRest();
         return  view;
+    }
+    private void initializeRest() {
+        celebrityFragmentBioLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        celebrityFragmentBioRecycler.setLayoutManager(celebrityFragmentBioLayoutManager);
+        celebrityBioAdapter = new CelebrityBioAdapter(getActivity(),fragmentManager);
+        celebrityFragmentBioRecycler.setAdapter(celebrityBioAdapter);
+    }
+
+    private void initializeViews() {
+        celebrityFragmentBioRecycler=(RecyclerView)view.findViewById(R.id.fragment_celebrity_bio_recycler);
+        fragmentManager=getActivity().getSupportFragmentManager();
     }
 }
