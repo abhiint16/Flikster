29ee058d1d43ac36c7104a6d74674a92c6fc089c
bheema <bheemarao.unnamatla@immacus.in>
2017-10-17 16:34:45 +0530

    initialized login email activity and implemented onclick event

---
+++ app/src/main/java/com/flikster/Authentication/LoginEmailActivity.java
@@ -0,0 +1,30 @@
+package com.flikster.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.flikster.R;
+
+public class LoginEmailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnEmailOtpSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_email);
+        initializeButton();
+    }
+
+    private void initializeButton() {
+        btnEmailOtpSend = (Button)findViewById(R.id.btn_login_otp_send);
+        btnEmailOtpSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
