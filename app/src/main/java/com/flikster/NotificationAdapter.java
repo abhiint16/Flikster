76ab09bb10c607738fddaa41dcc827cd3e0a446e
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-20 13:04:08 +0530

    initializing Notification Adapter

---
+++ app/src/main/java/com/flikster/NotificationAdapter.java
@@ -0,0 +1,32 @@
+package com.flikster;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abhishek on 20-10-2017.
+ */
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+    @Override
+    public NotificationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(NotificationAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/flikster/NotificationFragment.java
+++ app/src/main/java/com/flikster/NotificationFragment.java
@@ -3,6 +3,8 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +16,26 @@ import android.widget.TextView;
 
 public class NotificationFragment extends Fragment {
     View view;
-    TextView textView;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    NotificationAdapter notificationAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_fashion,container,false);
-        textView=(TextView)view.findViewById(R.id.tvf);
-        textView.setText("Notification page is working");
+        view=inflater.inflate(R.layout.fragment_feed,container,false);
+        initializeViews();
+        initializeRest();
         return view;
+    }
+
+    private void initializeRest() {
+        layoutManager=new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        notificationAdapter=new NotificationAdapter();
+        recyclerView.setAdapter(notificationAdapter);
+    }
+
+    private void initializeViews() {
+        recyclerView=(RecyclerView)view.findViewById(R.id.recyclerview);
     }
 }
