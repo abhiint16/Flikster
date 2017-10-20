e206fd017a0d606801f114162a6a3fa47bd9929a
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-20 17:19:08 +0530

    initializing MyAccountFragment java file

--- app/src/main/java/com/flikster/MyAccountFragment.java
+++ app/src/main/java/com/flikster/MyAccountFragment.java
@@ -2,7 +2,11 @@ package com.flikster;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +17,34 @@ import android.widget.TextView;
  */
 
 public class MyAccountFragment extends Fragment {
+    Toolbar toolbar;
+    ViewPager viewPager;
     View view;
     TextView textView;
+    TabLayout tabLayout;
+    CelebrityAdapter celebrityAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_fashion,container,false);
-        textView=(TextView)view.findViewById(R.id.tvf);
-        textView.setText("MyAccount is working fine");
+        view=inflater.inflate(R.layout.fragment_my_account,container,false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        initializeViews();
+        initializeRest();
         return view;
+    }
+
+    private void initializeRest() {
+        celebrityAdapter = new CelebrityAdapter(getChildFragmentManager());
+        viewPager.setAdapter(celebrityAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void initializeViews() {
+        viewPager=(ViewPager)view.findViewById(R.id.fragment_my_account_pager);
+        tabLayout=(TabLayout)view.findViewById(R.id.fragment_my_account_tablayout);
+        tabLayout.setBackgroundColor(getResources().getColor(R.color.white));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.dark_grey), getResources().getColor(R.color.black));
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
+
     }
 }
