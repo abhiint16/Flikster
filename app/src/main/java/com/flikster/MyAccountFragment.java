1b4b01bd29b2af2534b466a215876896e0a4423b
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-21 10:40:49 +0530

    adding toolbar icons

--- app/src/main/java/com/flikster/MyAccountFragment.java
+++ app/src/main/java/com/flikster/MyAccountFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -42,6 +47,7 @@ public class MyAccountFragment extends Fragment {
     private void initializeViews() {
         viewPager=(ViewPager)view.findViewById(R.id.fragment_my_account_pager);
         tabLayout=(TabLayout)view.findViewById(R.id.fragment_my_account_tablayout);
+        toolbar=(Toolbar)view.findViewById(R.id.fragment_my_account_toolbar);
         tabLayout.setBackgroundColor(getResources().getColor(R.color.white));
         tabLayout.setTabTextColors(getResources().getColor(R.color.dark_grey), getResources().getColor(R.color.black));
         tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
