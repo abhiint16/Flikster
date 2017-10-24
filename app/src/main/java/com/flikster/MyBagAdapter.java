b98385e9ae1685565032fb50a09642af393b1be9
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-24 16:01:32 +0530

    adding order summary card

--- app/src/main/java/com/flikster/MyBagAdapter.java
+++ app/src/main/java/com/flikster/MyBagAdapter.java
@@ -15,7 +15,7 @@ import java.util.List;
  * Created by abhishek on 24-10-2017.
  */
 
-public class MyBagAdapter extends RecyclerView.Adapter<MyBagAdapter.ViewHolder> {
+public class MyBagAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     List<Integer> color=new ArrayList<>();
     List<String> title=new ArrayList<>();
     List<String> desc=new ArrayList<>();
@@ -31,17 +31,28 @@ public class MyBagAdapter extends RecyclerView.Adapter<MyBagAdapter.ViewHolder> 
     }
 
     @Override
-    public MyBagAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_mybag_recycler_item,parent,false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType==0)
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_mybag_recycler_item,parent,false);
+            return new ViewHolder1(view);
+        }
+        else
+        {
+            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.card_fragment_mybag_order_summary,parent,false);
+            return new ViewHolder2(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(MyBagAdapter.ViewHolder holder, int position) {
-        holder.notification_item_img.setImageResource(img.get(position));
-        holder.notification_item_title.setText(title.get(position));
-        holder.notification_item_desc.setText(desc.get(position));
-        holder.notification_item_color.setBackgroundColor(color.get(position));
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType()==0)
+        {
+            ((ViewHolder1)holder).notification_item_img.setImageResource(img.get(position));
+            ((ViewHolder1)holder).notification_item_title.setText(title.get(position));
+            ((ViewHolder1)holder).notification_item_desc.setText(desc.get(position));
+            ((ViewHolder1)holder).notification_item_color.setBackgroundColor(color.get(position));
+        }
     }
 
     @Override
@@ -49,16 +60,30 @@ public class MyBagAdapter extends RecyclerView.Adapter<MyBagAdapter.ViewHolder> 
         return 4;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        if (position!=3)
+            return 0;
+        else
+            return 1;
+    }
+
+    public class ViewHolder1 extends RecyclerView.ViewHolder {
         TextView notification_item_title,notification_item_desc;
         ImageView notification_item_img;
         Button notification_item_color;
-        public ViewHolder(View itemView) {
+        public ViewHolder1(View itemView) {
             super(itemView);
             notification_item_color=(Button)itemView.findViewById(R.id.notification_item_color);
             notification_item_desc=(TextView)itemView.findViewById(R.id.notification_item_desc);
             notification_item_title=(TextView)itemView.findViewById(R.id.notification_item_title);
             notification_item_img=(ImageView)itemView.findViewById(R.id.notification_item_img);
+        }
+    }
+    public class ViewHolder2 extends RecyclerView.ViewHolder{
+        public ViewHolder2(View itemView)
+        {
+            super(itemView);
         }
     }
 }
--- app/src/main/java/com/flikster/MyBagFragment.java
+++ app/src/main/java/com/flikster/MyBagFragment.java
@@ -3,6 +3,7 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ public class MyBagFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_toolbar_plus_recyclerview,container,false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
         initializeViews();
         initializeRest();
         return view;
@@ -36,5 +38,11 @@ public class MyBagFragment extends Fragment {
 
     private void initializeViews() {
         recyclerView=(RecyclerView)view.findViewById(R.id.fragment_toolbar_plus_recyclerview_recycler_view);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 }
