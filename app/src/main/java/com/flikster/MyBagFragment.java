50695ea53227924ddd24f8de8efaa96b6ff15a99
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-24 14:41:34 +0530

    setting adapter for MyBag recycler

--- app/src/main/java/com/flikster/MyBagFragment.java
+++ app/src/main/java/com/flikster/MyBagFragment.java
@@ -3,6 +3,8 @@ package com.flikster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +15,26 @@ import android.view.ViewGroup;
 
 public class MyBagFragment extends Fragment {
     View view;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    MyBagAdapter myBagAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_toolbar_plus_recyclerview,container,false);
+        initializeViews();
+        initializeRest();
         return view;
+    }
+
+    private void initializeRest() {
+        layoutManager=new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        myBagAdapter=new MyBagAdapter();
+        recyclerView.setAdapter(myBagAdapter);
+    }
+
+    private void initializeViews() {
+        recyclerView=(RecyclerView)view.findViewById(R.id.fragment_toolbar_plus_recyclerview_recycler_view);
     }
 }
