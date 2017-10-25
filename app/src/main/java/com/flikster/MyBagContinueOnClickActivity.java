8afa7e2ef69fc838081912a0571135dea1174dd9
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-25 13:06:54 +0530

    changing payment pipeline to activity and adding to manifest

--- app/src/main/java/com/flikster/MyBagContinueOnClickActivity.java
+++ app/src/main/java/com/flikster/MyBagContinueOnClickActivity.java
@@ -2,10 +2,7 @@ package com.flikster;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,34 +15,24 @@ import android.widget.ImageButton;
 
 public class MyBagContinueOnClickFragment extends Fragment implements View.OnClickListener {
     View view;
-    ViewPager viewPager;
-    TabLayout tabLayout;
-    MyBagContinueOnClickFragmentAdapter myBagContinueOnClickFragmentAdapter;
     ImageButton fragment_mybag_continue_onclick_toolbar_back_navigation;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_mybag_continue_onclick,container,false);
+        view=inflater.inflate(R.layout.activity_mybag_continue_onclick,container,false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        addressFragmentLaunch();
         initializeView();
         initializeRest();
         return view;
     }
 
     private void initializeRest() {
-        myBagContinueOnClickFragmentAdapter=new MyBagContinueOnClickFragmentAdapter(getChildFragmentManager());
-        viewPager.setAdapter(myBagContinueOnClickFragmentAdapter);
-        tabLayout.setupWithViewPager(viewPager);
         fragment_mybag_continue_onclick_toolbar_back_navigation.setOnClickListener(this);
     }
 
     private void initializeView() {
-        viewPager=(ViewPager)view.findViewById(R.id.fragment_mybag_continue_onclick_viewpager);
-        tabLayout=(TabLayout)view.findViewById(R.id.fragment_mybag_continue_onclick_tab);
         fragment_mybag_continue_onclick_toolbar_back_navigation=(ImageButton)view.findViewById(R.id.fragment_mybag_continue_onclick_toolbar_back_navigation);
-        tabLayout.setBackgroundColor(getResources().getColor(R.color.white));
-        tabLayout.setTabTextColors(getResources().getColor(R.color.dark_grey), getResources().getColor(R.color.black));
-        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
     }
 
     @Override
@@ -60,5 +47,13 @@ public class MyBagContinueOnClickFragment extends Fragment implements View.OnCli
         {
             getFragmentManager().popBackStackImmediate();
         }
+    }
+
+    private void addressFragmentLaunch()
+    {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.payment_container,new PaymentAddressFragment())
+                .addToBackStack("")
+                .commit();
     }
 }
