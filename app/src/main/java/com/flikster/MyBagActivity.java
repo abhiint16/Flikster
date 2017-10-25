d3a4319f92647ca5e10e4e2373003681e68711e2
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-25 12:49:00 +0530

    changing id names and adding activity to manifest

--- app/src/main/java/com/flikster/MyBagActivity.java
+++ app/src/main/java/com/flikster/MyBagActivity.java
@@ -2,7 +2,6 @@ package com.flikster;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,58 +15,47 @@ import android.widget.ImageButton;
  * Created by abhishek on 24-10-2017.
  */
 
-public class MyBagFragment extends Fragment implements View.OnClickListener {
+public class MyBagActivity extends AppCompatActivity implements View.OnClickListener {
     View view;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     MyBagAdapter myBagAdapter;
-    ImageButton fragment_toolbar_plus_recyclerview_back_navigation;
-    Button fragment_toolbar_plus_recyclerview_continue_button;
-    @Nullable
+    ImageButton activity_my_bag_toolbar_back_navigation_btn;
+    Button activity_my_bag_bottom_continue_btn;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_toolbar_plus_recyclerview,container,false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bag);
         initializeViews();
         initializeRest();
-        return view;
     }
 
     private void initializeRest() {
-        layoutManager=new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(layoutManager);
         myBagAdapter=new MyBagAdapter();
         recyclerView.setAdapter(myBagAdapter);
-        fragment_toolbar_plus_recyclerview_back_navigation.setOnClickListener(this);
-        fragment_toolbar_plus_recyclerview_continue_button.setOnClickListener(this);
+        activity_my_bag_toolbar_back_navigation_btn.setOnClickListener(this);
+        activity_my_bag_bottom_continue_btn.setOnClickListener(this);
     }
 
     private void initializeViews() {
-        recyclerView=(RecyclerView)view.findViewById(R.id.fragment_toolbar_plus_recyclerview_recycler_view);
-        fragment_toolbar_plus_recyclerview_continue_button=(Button)view.findViewById(R.id.fragment_toolbar_plus_recyclerview_continue_button);
-        fragment_toolbar_plus_recyclerview_back_navigation=(ImageButton)view.findViewById(R.id.fragment_toolbar_plus_recyclerview_back_navigation);
+        recyclerView=(RecyclerView)findViewById(R.id.activity_my_bag_recycler_view);
+        activity_my_bag_bottom_continue_btn=(Button)findViewById(R.id.activity_my_bag_bottom_continue_btn);
+        activity_my_bag_toolbar_back_navigation_btn=(ImageButton)findViewById(R.id.activity_my_bag_toolbar_back_navigation_btn);
 
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.fragment_toolbar_plus_recyclerview_back_navigation)
+        if(view.getId()==R.id.activity_my_bag_toolbar_back_navigation_btn)
         {
             getFragmentManager().popBackStackImmediate();
         }
-        else if(view.getId()==R.id.fragment_toolbar_plus_recyclerview_continue_button)
+        else if(view.getId()==R.id.activity_my_bag_bottom_continue_btn)
         {
-            getFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_container,new MyBagContinueOnClickFragment())
-                    .addToBackStack("")
-                    .commit();
+
         }
     }
 }
