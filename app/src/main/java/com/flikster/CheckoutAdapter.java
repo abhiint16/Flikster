3ccf38c8cc4859ca1dc6c46cf7b6830e38f605cf
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-25 17:02:34 +0530

    adding checkout layout

--- app/src/main/java/com/flikster/CheckoutAdapter.java
+++ app/src/main/java/com/flikster/CheckoutAdapter.java
@@ -1,31 +1,77 @@
 package com.flikster;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by abhishek on 25-10-2017.
  */
 
-public class CheckoutAdapter extends RecyclerView.Adapter<CheckoutAdapter.ViewHolder> {
+public class CheckoutAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
-    public CheckoutAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(viewType==0)
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_checkout_customer_details,parent,false);
+            return new ViewHolder1(view);
+        }
+        else if(viewType==1)
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_mybag_recycler_item,parent,false);
+            return new ViewHolder2(view);
+        }
+        else if(viewType==2)
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_checkout_promo,parent,false);
+            return new ViewHolder3(view);
+        }
+        else
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_fragment_mybag_order_summary,parent,false);
+            return new ViewHolder4(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(CheckoutAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder.getItemViewType()==1)
+        {
+            ((ViewHolder2)holder).notification_item_cancel_btn.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 4;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    public class ViewHolder1 extends RecyclerView.ViewHolder {
+        public ViewHolder1(View itemView) {
+            super(itemView);
+        }
+    }
+    public class ViewHolder2 extends RecyclerView.ViewHolder {
+        ImageButton notification_item_cancel_btn;
+        public ViewHolder2(View itemView) {
+            super(itemView);
+            notification_item_cancel_btn=(ImageButton)itemView.findViewById(R.id.notification_item_cancel_btn);
+        }
+    }
+    public class ViewHolder3 extends RecyclerView.ViewHolder {
+        public ViewHolder3(View itemView) {
+            super(itemView);
+        }
+    }
+    public class ViewHolder4 extends RecyclerView.ViewHolder {
+        public ViewHolder4(View itemView) {
             super(itemView);
         }
     }
--- app/src/main/java/com/flikster/MyBagContinueOnClickActivity.java
+++ app/src/main/java/com/flikster/MyBagContinueOnClickActivity.java
@@ -2,13 +2,11 @@ package com.flikster;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 /**
  * Created by abhishek on 24-10-2017.
@@ -17,6 +15,8 @@ import android.widget.ImageButton;
 public class MyBagContinueOnClickActivity extends AppCompatActivity implements View.OnClickListener {
     ImageButton activity_mybag_continue_onclick_toolbar_back_navigation_btn;
     FragmentManager fragmentManager;
+    LinearLayout activity_mybag_continue_onclick_tabs_address_linear,activity_mybag_continue_onclick_tabs_checkout_linear
+            ,activity_mybag_continue_onclick_tabs_payment_linear;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,10 +30,16 @@ public class MyBagContinueOnClickActivity extends AppCompatActivity implements V
     private void initializeRest() {
         activity_mybag_continue_onclick_toolbar_back_navigation_btn.setOnClickListener(this);
         fragmentManager=getSupportFragmentManager();
+        activity_mybag_continue_onclick_tabs_address_linear.setOnClickListener(this);
+        activity_mybag_continue_onclick_tabs_checkout_linear.setOnClickListener(this);
+        activity_mybag_continue_onclick_tabs_payment_linear.setOnClickListener(this);
     }
 
     private void initializeView() {
+        activity_mybag_continue_onclick_tabs_address_linear=(LinearLayout)findViewById(R.id.activity_mybag_continue_onclick_tabs_address_linear);
         activity_mybag_continue_onclick_toolbar_back_navigation_btn = (ImageButton) findViewById(R.id.activity_mybag_continue_onclick_toolbar_back_navigation_btn);
+        activity_mybag_continue_onclick_tabs_checkout_linear=(LinearLayout)findViewById(R.id.activity_mybag_continue_onclick_tabs_checkout_linear);
+        activity_mybag_continue_onclick_tabs_payment_linear=(LinearLayout)findViewById(R.id.activity_mybag_continue_onclick_tabs_payment_linear);
     }
 
     @Override
@@ -41,11 +47,26 @@ public class MyBagContinueOnClickActivity extends AppCompatActivity implements V
         if (view.getId() == R.id.activity_mybag_continue_onclick_toolbar_back_navigation_btn) {
             getFragmentManager().popBackStackImmediate();
         }
+        else if(view.getId()==R.id.activity_mybag_continue_onclick_tabs_address_linear)
+        {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.activity_mybag_continue_onclick_container,new AddressFragment())
+                    .commit();
+        }
+        else if(view.getId()==R.id.activity_mybag_continue_onclick_tabs_checkout_linear)
+        {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.activity_mybag_continue_onclick_container,new CheckoutFragment())
+                    .commit();
+        }
+        else if(view.getId()==R.id.activity_mybag_continue_onclick_tabs_payment_linear)
+        {
+        }
     }
 
     private void addressFragmentLaunch() {
         fragmentManager.beginTransaction()
-                .replace(R.id.activity_mybag_continue_onclick_container,new PaymentAddressFragment())
+                .replace(R.id.activity_mybag_continue_onclick_container,new AddressFragment())
                 .commit();
     }
 }
