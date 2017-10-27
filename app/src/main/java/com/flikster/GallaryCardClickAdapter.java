10124619dd939ecdb51c6a1264122846b71ca098
Abhishek Kumar <abhishekint16@gmail.com>
2017-10-27 15:19:43 +0530

    adding bottom recommended g gallary carousel

--- app/src/main/java/com/flikster/GallaryCardClickAdapter.java
+++ app/src/main/java/com/flikster/GallaryCardClickAdapter.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -21,10 +23,13 @@ import com.bumptech.glide.Glide;
  * Created by abhishek on 05-10-2017.
  */
 
-public class GallaryCardClickAdapter extends RecyclerView.Adapter<GallaryCardClickAdapter.ViewHolder> {
+public class GallaryCardClickAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     GlobalData globalData;
     Context context;
     FragmentManager fragmentManager;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    CelebrityBioAdapterImagesViewHolder celebrityBioAdapterImagesViewHolder=new CelebrityBioAdapterImagesViewHolder();
     public GallaryCardClickAdapter(Context context, FragmentManager fragmentManager) {
         this.context=context;
         globalData=new GlobalData();
@@ -32,28 +37,52 @@ public class GallaryCardClickAdapter extends RecyclerView.Adapter<GallaryCardCli
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_gallary_item_onclick_recycler_item,parent,false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(viewType==0)
+        {
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_gallary_item_onclick_recycler_item,parent,false);
+            return new ViewHolder1(view);
+        }
+        else
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_celebrity_bio_videos_carousel, parent, false);
+            return new ViewHolder2(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         //holder.gallary_recycler_item_img.setContentDescription("aaaaaaaaaaaa");
         //Glide.with(holder.itemView.getContext()).load(globalData.imag.get(position)).into(holder.gallary_recycler_item_img);
-        holder.gallary_recycler_item_img.setImageResource(globalData.pooja.get(position));
-
+        if(holder.getItemViewType()==0)
+        {
+            ((ViewHolder1)holder).gallary_recycler_item_img.setImageResource(globalData.pooja.get(position));
+        }
+        else
+        {
+            ((ViewHolder2)holder).textView.setText("Recommended Gallary");
+            layoutManager=new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false);
+            ((ViewHolder2)holder).recyclerView.setLayoutManager(layoutManager);
+            ((ViewHolder2)holder).recyclerView.setAdapter(celebrityBioAdapterImagesViewHolder);
+        }
     }
 
     @Override
     public int getItemCount() {
-        Log.e(" "+globalData.a," "+globalData.a);
-        return globalData.pooja.size();
+        return globalData.pooja.size()+1;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    @Override
+    public int getItemViewType(int position) {
+        if((position)==globalData.pooja.size())
+            return 1;
+        else
+            return 0;
+    }
+
+    public class ViewHolder1 extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView gallary_recycler_item_img;
-        public ViewHolder(View itemView) {
+        public ViewHolder1(View itemView) {
             super(itemView);
             gallary_recycler_item_img=(ImageView)itemView.findViewById(R.id.gallary_recycler_item_img);
             itemView.setOnClickListener(this);
@@ -71,6 +100,18 @@ public class GallaryCardClickAdapter extends RecyclerView.Adapter<GallaryCardCli
             window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
             window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.show();
+        }
+    }
+
+    public  class ViewHolder2 extends RecyclerView.ViewHolder
+    {
+        TextView textView;
+        RecyclerView recyclerView;
+        public ViewHolder2(View itemView)
+        {
+            super(itemView);
+            recyclerView=(RecyclerView)itemView.findViewById(R.id.rc);
+            recyclerView=(RecyclerView) itemView.findViewById(R.id.card_celebrity_bio_video_carousel_recycler);
         }
     }
 }
