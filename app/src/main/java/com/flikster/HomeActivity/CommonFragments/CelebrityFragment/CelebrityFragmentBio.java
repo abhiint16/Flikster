320d01f10d3d280e4b2720fb3dee811d0353cdbe
Abhishek Kumar <abhishekint16@gmail.com>
2017-11-11 12:10:12 +0530

    writing retrofit code in inner fragments for respective API

--- app/src/main/java/com/flikster/HomeActivity/CommonFragments/CelebrityFragment/CelebrityFragmentBio.java
+++ app/src/main/java/com/flikster/HomeActivity/CommonFragments/CelebrityFragment/CelebrityFragmentBio.java
@@ -6,11 +6,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.flikster.HomeActivity.ApiClient;
+import com.flikster.HomeActivity.ApiInterface;
+import com.flikster.HomeActivity.CommonFragments.MovieFragment.MovieData;
+import com.flikster.HomeActivity.CommonFragments.MovieFragment.MovieInfoAdapter;
 import com.flikster.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by abhishek on 12-10-2017.
@@ -22,19 +33,43 @@ public class CelebrityFragmentBio extends Fragment{
     RecyclerView.LayoutManager celebrityFragmentBioLayoutManager;
     CelebrityBioAdapter celebrityBioAdapter;
     FragmentManager fragmentManager;
+    Bundle bundle;
+    ApiInterface apiInterface;
+    List<CelebrityData.CelebrityInnerData> items;
+    String slug;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_common_recyclerview,container,false);
         initializeViews();
+        bundle=getArguments();
+        this.slug=bundle.getString("slug");
+        retrofitInit();
         initializeRest();
         return  view;
     }
+
+    private void retrofitInit() {
+        apiInterface = ApiClient.getClient("http://apiv3.flikster.com/v3/celeb-ms/getCelebBySlug/").create(ApiInterface.class);
+        Call<CelebrityData> call = apiInterface.getCelebrityData("http://apiv3.flikster.com/v3/celeb-ms/getCelebBySlug/"+slug);
+        call.enqueue(new Callback<CelebrityData>() {
+            @Override
+            public void onResponse(Call<CelebrityData> call, Response<CelebrityData> response) {
+                items = response.body().getItems();
+                celebrityBioAdapter = new CelebrityBioAdapter(getActivity(), fragmentManager,items);
+                celebrityFragmentBioRecycler.setAdapter(celebrityBioAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<CelebrityData> call, Throwable t) {
+                Log.e("vvvvvvvvvv","vv"+call+t);
+            }
+        });
+    }
+
     private void initializeRest() {
         celebrityFragmentBioLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         celebrityFragmentBioRecycler.setLayoutManager(celebrityFragmentBioLayoutManager);
-        celebrityBioAdapter = new CelebrityBioAdapter(getActivity(),fragmentManager);
-        celebrityFragmentBioRecycler.setAdapter(celebrityBioAdapter);
     }
 
     private void initializeViews() {
--- app/src/main/java/com/flikster/HomeActivity/CommonFragments/MovieFragment/MovieFragmentInfo.java
+++ app/src/main/java/com/flikster/HomeActivity/CommonFragments/MovieFragment/MovieFragmentInfo.java
@@ -6,12 +6,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.flikster.HomeActivity.ApiClient;
+import com.flikster.HomeActivity.ApiInterface;
 import com.flikster.HomeActivity.CommonFragments.MovieFragment.MovieInfoAdapter;
+import com.flikster.HomeActivity.FeedFragment.FeedRecyclerAdapter;
 import com.flikster.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by abhishek on 12-10-2017.
@@ -23,21 +33,48 @@ public class MovieFragmentInfo extends Fragment {
     RecyclerView.LayoutManager movieFragmentInfoLayoutManager;
     MovieInfoAdapter movieInfoAdapter;
     FragmentManager fragmentManager;
+    Bundle bundle;
+    ApiInterface apiInterface;
+    List<MovieData.MovieInnerData> items;
+    String slug;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_common_recyclerview, container, false);
         initializeViews();
+        bundle=getArguments();
+        this.slug=bundle.getString("slug");
+        retrofitInit();
         initializeRest();
         return view;
     }
 
+    private void retrofitInit() {
+        apiInterface = ApiClient.getClient("http://apiv3.flikster.com/v3/movie-ms/getMovieBySlug/").create(ApiInterface.class);
+        Log.e("mmmmmmm","mmmmmmm");
+        Call<MovieData> call = apiInterface.getMovieData("http://apiv3.flikster.com/v3/movie-ms/getMovieBySlug/"+slug);
+        call.enqueue(new Callback<MovieData>() {
+            @Override
+            public void onResponse(Call<MovieData> call, Response<MovieData> response) {
+                items = response.body().getItems();
+                movieInfoAdapter = new MovieInfoAdapter(getActivity(), fragmentManager,items);
+                movieFragmentInfoRecycler.setAdapter(movieInfoAdapter);
+                Log.e("xxxxxxxxx","xxx"+items.size());
+                Log.e("xxxxxxxxx","yyyy"+items);
+            }
+
+            @Override
+            public void onFailure(Call<MovieData> call, Throwable t) {
+                Log.e("vvvvvvvvvv","vv"+call+t);
+            }
+        });
+    }
+
+
     private void initializeRest() {
         movieFragmentInfoLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         movieFragmentInfoRecycler.setLayoutManager(movieFragmentInfoLayoutManager);
-        movieInfoAdapter = new MovieInfoAdapter(getActivity(), fragmentManager);
-        movieFragmentInfoRecycler.setAdapter(movieInfoAdapter);
     }
 
     private void initializeViews() {
