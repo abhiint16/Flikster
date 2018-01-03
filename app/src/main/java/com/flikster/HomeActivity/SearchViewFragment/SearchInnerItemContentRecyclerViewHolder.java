d7d9ae43e647087b61edc28c11ad81ea7ed10e23
Abhishek Kumar <abhishekint16@gmail.com>
2018-01-03 20:05:26 +0530

    implementing content item click

--- app/src/main/java/com/flikster/HomeActivity/SearchViewFragment/SearchInnerItemContentRecyclerViewHolder.java
+++ app/src/main/java/com/flikster/HomeActivity/SearchViewFragment/SearchInnerItemContentRecyclerViewHolder.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.flikster.HomeActivity.CommonFragments.NewsFragment.NewsOnClickFragment;
 import com.flikster.HomeActivity.GlobalSearchGetData;
 import com.flikster.R;
+import com.flikster.Util.SharedPrefsUtil;
 
 import java.util.List;
 
@@ -20,7 +22,7 @@ public class SearchInnerItemContentRecyclerViewHolder extends RecyclerView.Adapt
     Context context;
     List<GlobalSearchGetData.SearchContentData> searchContentDatas;
     SearchViewFragment.SearchViewToFrag searchViewToFrag;
-
+    String userId="null";
     public SearchInnerItemContentRecyclerViewHolder(Context context, List<GlobalSearchGetData.SearchContentData> searchContentDatas,
                                                     SearchViewFragment.SearchViewToFrag searchViewToFrag) {
         this.context=context;
@@ -37,6 +39,9 @@ public class SearchInnerItemContentRecyclerViewHolder extends RecyclerView.Adapt
 
     @Override
     public void onBindViewHolder(SearchInnerItemContentRecyclerViewHolder.ViewHolder holder, int position) {
+        if (SharedPrefsUtil.getStringPreference(context, "USER_ID") != null && !SharedPrefsUtil.getStringPreference(context, "USER_ID").isEmpty()) {
+            userId = SharedPrefsUtil.getStringPreference(context, "USER_ID");
+        }
         holder.textView.setText(searchContentDatas.get(position).getName());
     }
 
@@ -45,11 +50,26 @@ public class SearchInnerItemContentRecyclerViewHolder extends RecyclerView.Adapt
         return searchContentDatas.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView textView;
         public ViewHolder(View itemView) {
             super(itemView);
             textView=(TextView)itemView.findViewById(R.id.searchview_inner_recycler_item);
+            textView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            searchViewToFrag.newsCardOnClick("",
+                    "",
+                    "",
+                    searchContentDatas.get(getAdapterPosition()).getProfilePic(),
+                    searchContentDatas.get(getAdapterPosition()).getName(),
+                    searchContentDatas.get(getAdapterPosition()).getName(),
+                    new NewsOnClickFragment(),
+                    searchContentDatas.get(getAdapterPosition()).getContentType(),
+                    userId,
+                    searchContentDatas.get(getAdapterPosition()).getId());
         }
     }
 }
